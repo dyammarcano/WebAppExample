22c57dc5b3bc94369f59a2f21b0f7288b0ff25ad
Dyam Marcano <dyam.marcano@gmail.com>
2019-07-24 13:53:43 -0300

    base

---
+++ WebAppExample/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(WebAppExample.Startup))]
+
+namespace WebAppExample
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
